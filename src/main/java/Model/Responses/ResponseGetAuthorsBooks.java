f164f7ab21234c6a8197b51ceed581fe699c037e
Ruslan <ruslan24krut@gmail.com>
2024-07-03 21:39:31 +0300

    Добавил задание 4.4.6

--- src/main/java/Model/Responses/ResponseGetAuthorsBooks.java
+++ src/main/java/Model/Responses/ResponseGetAuthorsBooks.java
@@ -6,15 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ResponseGetAuthorsBooks {
     private int errorCode;
     private String errorMessage;
     private String errorDetails;
-    private List<Book> books;
 
     @Data
     public class Book {
--- src/test/java/LibraryService/TestLibraryServicePostRequest.java
+++ src/test/java/LibraryService/TestLibraryServicePostRequest.java
@@ -14,18 +14,37 @@ import static LibraryService.GenerateTestData.*;
 import static junit.framework.Assert.assertEquals;
 
 @Epic("Получение статус кодов на запрос POST")
-@Story("Проверяется успешный POST запрос")
+@Story("Проверяются статус коды 200, 400 и 409")
 public class TestLibraryServicePostRequest {
     private final int STATUS_CODE_FOR_SUCCESS_POST = 201;
+    private final int STATUS_CODE_FOR_NULL_POST = 400;
+    private final int STATUS_CODE_FOR_INCORRECT_POST = 409;
 
     @Test
-    @DisplayName("Статус-код, при добавлении новой книги с существующим Author id")
-    @Description("Проверка того, что при вводе существующего author id и корректного bookTitle в post запрос, будет выдаваться 200 статус-код")
+    @DisplayName("Статус-код при добавлении новой книги с существующим Author id")
+    @Description("Проверка того что при вводе существующего author id в post запрос будет выдаваться 200 статус-код")
     public void testStatusCodePutAuthorsBookWithCorrectData() {
         Author author = generateNewAuthor();
         String bookTitle = generateBookTitle();
 
         Response response = RequestSender.ResponsePostBook(new RequestPostNewBook(bookTitle, author));
         assertEquals(STATUS_CODE_FOR_SUCCESS_POST, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Статус-код при добавлении новой книги без ввода обязательных параметров")
+    @Description("Проверка того что при отсутствии ввода author id и bookTitle в post запрос будет выдаваться 400 статус-код")
+    public void testStatusCodePutAuthorsBookWithNullData() {
+        Response response = RequestSender.ResponsePostBook(new RequestPostNewBook());
+        assertEquals(STATUS_CODE_FOR_NULL_POST, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Статус-код при добавлении новой книги с несуществующим Author id")
+    @Description("Проверка того что при вводе несуществующего author id в post запрос будет выдаваться 409 статус-код")
+    public void testStatusCodePutAuthorsBookWithIncorrectAuthor() {
+        Author author = new Author();
+        Response response = RequestSender.ResponsePostBook(new RequestPostNewBook("test", author));
+        assertEquals(STATUS_CODE_FOR_INCORRECT_POST, response.getStatusCode());
     }
 }
